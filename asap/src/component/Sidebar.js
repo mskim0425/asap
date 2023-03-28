15d055f19998cee2c103b1d91e00656912f36cf1
leeseoshim <leeseoit@gmail.com>
2023-03-28 18:53:34 +0900

    재고관리 와이어프레임

--- asap/src/component/Sidebar.js
+++ asap/src/component/Sidebar.js
@@ -1,23 +1,27 @@
 import { useNavigate } from "react-router-dom";
-import "./Sidebar.css"
+import "./Sidebar.css";
 
 export default function Sidebar() {
-  const navigate = useNavigate()
-  
-  const link = (e) => {
-    if(e.target.innerText === "Dashboard"){
-      navigate("/")
-    }else{
-      navigate(e.target.innerText)
-    }
-  }
+  const navigate = useNavigate();
 
-  const tab = ["Dashboard", "Stuff"]
+  const link = (e) => {
+    if (e.target.innerText === "Dashboard") {
+      navigate("/");
+    } else {
+      navigate(e.target.innerText);
+    }
+  };
+
+  const tab = ["Dashboard", "Stuff"];
 
   return (
     <section className="Sidebar">
-      {tab.map(el => {
-        return <div key={el} onClick={link}>{el}</div>
+      {tab.map((el) => {
+        return (
+          <div key={el} onClick={link} className="menu-button">
+            {el}
+          </div>
+        );
       })}
     </section>
   );
