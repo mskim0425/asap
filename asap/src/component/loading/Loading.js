653c85b373394a93ddfb56579f784fda3f4ca632
leeseoshim <leeseoit@gmail.com>
2023-04-03 15:53:33 +0900

    로딩 및 에러페이지 추가, 재고관리 페이지 api통신

---
+++ asap/src/component/loading/Loading.js
@@ -0,0 +1,16 @@
+import "./Loading.css";
+
+export default function Loading() {
+  return (
+    <div class="loading">
+      <div className="wrapper">
+        <span></span>
+        <span></span>
+        <span></span>
+        <div class="content">
+          <h2>Loading</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
