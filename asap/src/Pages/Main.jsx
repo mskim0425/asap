c708e64663975e23fc023f6979c4661ed2b1d5b5
leeseoshim <leeseoit@gmail.com>
2023-04-26 23:53:13 +0900

    메인페이지css

--- asap/src/Pages/Main.jsx
+++ asap/src/Pages/Main.jsx
@@ -1,4 +1,6 @@
 import "../style/Main.css";
+import anime from "animejs/lib/anime.es.js";
+import { useEffect } from "react";
 
 const Main = () => {
   const lists = document.querySelectorAll(".nav li");
@@ -13,6 +15,51 @@ const Main = () => {
     const header = document.querySelector("#header");
     header.classList.toggle("active");
   }
+
+  useEffect(() => {
+    let container = document.querySelector(".container2");
+    for (let i = 1; i <= 100; i++) {
+      let dot = document.createElement("div");
+      dot.classList.add("element");
+      container.appendChild(dot);
+    }
+
+    let dotAll = document.querySelectorAll(".element");
+
+    const animation = anime.timeline({
+      targets: dotAll,
+      // easing: "easeInOutExpo",
+      loop: true,
+      delay: anime.stagger(100, { grid: [10, 10], from: "center" }),
+    });
+    animation
+      .add({
+        rotateZ: 180,
+        transelateY: anime.stagger(-20, {
+          grid: [10, 10],
+          from: "center",
+          axis: "y",
+        }),
+        transelateX: anime.stagger(-20, {
+          grid: [10, 10],
+          from: "center",
+          axis: "x",
+          opacity: 1,
+        }),
+      })
+      .add({
+        borderRadius: 50,
+      })
+      .add({
+        scale: 0.2,
+        opacity: 1,
+      })
+
+      .add({
+        scale: 1,
+        opacity: 0,
+      });
+  }, []);
 
   return (
     <div>
@@ -44,7 +91,9 @@ const Main = () => {
           <div className="border"></div>
         </div>
       </section>
-      <section id="stuff"></section>
+      <section id="stuff">
+        <div className="container2"></div>
+      </section>
       <section id="contact">
         <div className="contactText">
           <h2>Ways To Contact Us</h2>
@@ -56,20 +105,36 @@ const Main = () => {
           </p>
         </div>
         <div className="content">
-          <a href="#none">
-            <i class="fa fa-envelope" aria-hidden="true"></i>
-            asap@gmail.com
-          </a>
-          <a href="#none">
-            <i className="fa fa-phone-square" aria-hidden="true"></i>
-            010-1234-5678
-          </a>
-          <a href="#none">
-            <i className="fa fa-github" aria-hidden="true"></i>
-            <a href="https://github.com/mskim0425/asap" target="blank">
-              git hub
+          <div className="contentWrapper">
+            <a href="#none">
+              <i className="fa fa-envelope" aria-hidden="true"></i>
+              asap@gmail.com
+              <span style={{ "--i": 0 }}></span>
+              <span style={{ "--i": 1 }}></span>
+              <span style={{ "--i": 2 }}></span>
+              <span style={{ "--i": 3 }}></span>
             </a>
-          </a>
+          </div>
+          <div className="contentWrapper">
+            <a href="#none">
+              <i className="fa fa-phone-square" aria-hidden="true"></i>
+              010-1234-5678
+              <span style={{ "--i": 0 }}></span>
+              <span style={{ "--i": 1 }}></span>
+              <span style={{ "--i": 2 }}></span>
+              <span style={{ "--i": 3 }}></span>
+            </a>
+          </div>
+          <div className="contentWrapper">
+            <a href="https://github.com/mskim0425/asap" target="blank">
+              <i className="fa fa-github" aria-hidden="true"></i>
+              git hub
+              <span style={{ "--i": 0 }}></span>
+              <span style={{ "--i": 1 }}></span>
+              <span style={{ "--i": 2 }}></span>
+              <span style={{ "--i": 3 }}></span>
+            </a>
+          </div>
         </div>
       </section>
     </div>
