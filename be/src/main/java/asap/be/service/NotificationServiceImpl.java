23bd9f74c8e3747f7d83b07c3c6ecc347ed69477
hyoreal <hyoreal51@gmail.com>
2023-05-10 00:59:54 +0900

    fix: remove sse header

--- be/src/main/java/asap/be/service/NotificationServiceImpl.java
+++ be/src/main/java/asap/be/service/NotificationServiceImpl.java
@@ -22,6 +22,7 @@ public class NotificationServiceImpl implements NotificationService {
 	private static final Long DEFAULT_TIMEOUT = 120L * 1000 * 60; // SSE 유효시간
 	private final EmitterRepository emitterRepository;
 	private List<SseEmitter> deadEmitter = new CopyOnWriteArrayList<>();
+
 	/**
 	 * SSE 통신
 	 */
@@ -34,11 +35,7 @@ public class NotificationServiceImpl implements NotificationService {
 		// 클라이언트의 sse 연결 요청에 응답하기 위한 SseEmitter 객체 생성
 		// 유효시간 지정으로 시간이 지나면 클라이언트에서 자동으로 재연결 요청함
 		SseEmitter emitter = emitterRepository.save(id, new SseEmitter(DEFAULT_TIMEOUT));
-		response.setHeader("Cache-Control", "no-cache");
-		response.setHeader("Connection", "keep-alive");
-		response.setHeader("Content-Type", "text/event-stream");
 		response.setHeader("X-Accel-Buffering", "no"); // NGINX PROXY 에서의 필요설정
-		response.setCharacterEncoding("UTF-8");
 
 		// SseEmitter 의 완료/시간초과/에러로 인한 전송 불가 시 sseEmitter 삭제
 		emitter.onCompletion(() -> emitterRepository.deleteAllStartByWithId(id));
@@ -61,18 +58,16 @@ public class NotificationServiceImpl implements NotificationService {
 
 	@Override
 	public void sendToClient(SseEmitter emitter, String id, Object data) {
-		List<SseEmitter> list = emitterRepository.getAll();
 
-		for(SseEmitter se :list) {
-			try {
-				se.send(SseEmitter.event()
-						.id(id)
-						.name("sse")
-						.data(data));
-			} catch (IOException e) {
-				deadEmitter.add(se);
-			}
+		try {
+			emitter.send(SseEmitter.event()
+					.id(id)
+					.name("sse")
+					.data(data));
+		} catch (IOException e) {
+			deadEmitter.add(emitter);
 		}
+
 	}
 
 	@Scheduled(fixedDelay = 10000)
